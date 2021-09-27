586622c5a811baa55effcfc4f7c6fd7978f0dd64
kishore-chandrasekar <91500847+kishore-chandrasekar@users.noreply.github.com>
2021-09-28 02:18:59 +0530

    Add files via upload

---
+++ print 2 inputs.js
@@ -0,0 +1,18 @@
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+    var a = userInput[0];
+    var b = userInput[1];
+    console.log(a);
+    console.log(b);
+    });
